0d1204a9a9c2403981f02b61502af408d4adec92
Demky <nemoaleksey20054@gmail.com>
2022-08-15 19:27:19 +0300

    update

--- src/PromoSystem/Layton/provider/Provider.php
+++ src/PromoSystem/Layton/provider/Provider.php
@@ -10,12 +10,14 @@ interface Provider {
 
     public function isCreated(string $promoCode): bool;
 
+    public function isCreator(Player $player): bool;
+
     public function create(Player $player, string $promoCode, int $amount): bool;
 
     public function delete(string $promoCode): bool;
 
     public function getData(string $promoCode): array;
-    
+
     public function getDataByPlayer(Player $player): array;
 
 }
