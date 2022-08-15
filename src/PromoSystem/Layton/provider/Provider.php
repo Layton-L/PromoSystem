9740a5bd9d66c01a42c9b4784c4fc8167757019d
Demky <nemoaleksey20054@gmail.com>
2022-08-15 19:25:57 +0300

    update

---
+++ src/PromoSystem/Layton/provider/Provider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PromoSystem\Layton\provider;
+
+use pocketmine\player\Player;
+
+interface Provider {
+
+    public function isCreated(string $promoCode): bool;
+
+    public function create(Player $player, string $promoCode, int $amount): bool;
+
+    public function delete(string $promoCode): bool;
+
+    public function getData(string $promoCode): array;
+    
+    public function getDataByPlayer(Player $player): array;
+
+}
