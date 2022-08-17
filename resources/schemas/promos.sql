298b6bcb8906f0e5fa3971a4f563c92ae03e62ef
Demky <nemoaleksey20054@gmail.com>
2022-08-17 14:54:08 +0300

    update

--- resources/schemas/promos.sql
+++ resources/schemas/promos.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS promos (
-    promo       TEXT    NOT     NULL,
+    promo       TEXT    NOT     NULL PRIMARY KEY,
     uses        INTEGER DEFAULT 0,
     max_uses    INTEGER NOT     NULL,
     action_time INTEGER NOT     NULL,
--- src/PromoSystem/Layton/provider/SQLite3Provider.php
+++ src/PromoSystem/Layton/provider/SQLite3Provider.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace PromoSystem\Layton\provider;
 
+use pocketmine\player\Player;
 use PromoSystem\Layton\PromoSystem;
 use SQLite3;
 
@@ -15,8 +16,146 @@ class SQLite3Provider implements Provider {
         $this->database = new SQLite3($plugin->getDataFolder() . "database.db");
 
         $this->database->exec(stream_get_contents($plugin->getResource("schemas/promos.sql")));
+        $this->database->exec(stream_get_contents($plugin->getResource("schemas/users.sql")));
+
         $this->database->exec("PRAGMA journal_mode=WAL;");
         $this->database->exec("PRAGMA synchronous=OFF;");
     }
 
+    public function isCreated(string $promo): bool {
+        $result = $this->database->query("SELECT * FROM `promos` WHERE `promo` = '" . $promo ."'");
+        return !empty($result->fetchArray(SQLITE3_ASSOC));
+    }
+
+    public function isTemporary(string $promo): bool {
+        $result = $this->database->query("SELECT `max_uses` FROM `promos` WHERE `promo` = '" . $promo ."'");
+        return $result->fetchArray(SQLITE3_ASSOC)["max_uses"] === -1;
+    }
+
+    public function isUsesLimited(string $promo): bool {
+        $result = $this->database->query("SELECT `action_time` FROM `promos` WHERE `promo` = '" . $promo ."'");
+        return $result->fetchArray(SQLITE3_ASSOC)["action_time"] === -1;
+    }
+
+    public function createTemporary(string $promo, int $actionTime, int $amount): bool {
+        $statement = $this->database->prepare("INSERT INTO `promos` (`promo`, `max_uses`, `action_time`, `amount`) VALUES (:promo, :max_uses, :action_time, :amount)");
+
+        $statement->bindValue(":promo", $promo);
+        $statement->bindValue(":max_uses", -1);
+        $statement->bindValue(":action_time", $actionTime);
+        $statement->bindValue(":amount", $amount);
+
+        $statement->execute();
+        return $this->database->changes() === 1;
+    }
+
+    public function createUsesLimited(string $promo, int $maxUses, int $amount): bool {
+        $statement = $this->database->prepare("INSERT INTO `promos` (`promo`, `max_uses`, `action_time`, `amount`) VALUES (:promo, :max_uses, :action_time, :amount)");
+
+        $statement->bindValue(":promo", $promo);
+        $statement->bindValue(":max_uses", $maxUses);
+        $statement->bindValue(":action_time", -1);
+        $statement->bindValue(":amount", $amount);
+
+        $statement->execute();
+        return $this->database->changes() === 1;
+    }
+
+    public function delete(string $promo): bool {
+        $this->database->query("DELETE FROM `promos` WHERE `promo` = '" . $promo ."'");
+        return $this->database->changes() === 1;
+    }
+
+    public function getUses(string $promo): int {
+        $result = $this->database->query("SELECT `uses` FROM `promos` WHERE `promo` = '" . $promo ."'");
+        return $result->fetchArray(SQLITE3_ASSOC)["uses"];
+    }
+
+    public function setUses(string $promo, int $uses): bool {
+        $statement = $this->database->prepare("UPDATE `promos` SET `uses` = :uses WHERE `promo` = :promo");
+
+        $statement->bindValue(":promo", $promo);
+        $statement->bindValue(":uses", $uses);
+
+        $statement->execute();
+        return $this->database->changes() === 1;
+    }
+
+    public function getMaxUses(string $promo): int {
+        $result = $this->database->query("SELECT `max_uses` FROM `promos` WHERE `promo` = '" . $promo ."'");
+        return $result->fetchArray(SQLITE3_ASSOC)["max_uses"];
+    }
+
+    public function setMaxUses(string $promo, int $maxUses): bool {
+        $statement = $this->database->prepare("UPDATE `promos` SET `max_uses` = :max_uses WHERE `promo` = :promo");
+
+        $statement->bindValue(":promo", $promo);
+        $statement->bindValue(":max_uses", $maxUses);
+
+        $statement->execute();
+        return $this->database->changes() === 1;
+    }
+
+    public function getActionTime(string $promo): int {
+        $result = $this->database->query("SELECT `action_time` FROM `promos` WHERE `promo` = '" . $promo ."'");
+        return $result->fetchArray(SQLITE3_ASSOC)["action_time"];
+    }
+
+    public function setActionTime(string $promo, int $actionTime): bool {
+        $statement = $this->database->prepare("UPDATE `promos` SET `action_time` = :action_time WHERE `promo` = :promo");
+
+        $statement->bindValue(":promo", $promo);
+        $statement->bindValue(":action_time", $actionTime);
+
+        $statement->execute();
+        return $this->database->changes() === 1;
+    }
+
+    public function getAmount(string $promo): int {
+        $result = $this->database->query("SELECT `amount` FROM `promos` WHERE `promo` = '" . $promo ."'");
+        return $result->fetchArray(SQLITE3_ASSOC)["amount"];
+    }
+
+    public function setAmount(string $promo, int $amount): bool {
+        $statement = $this->database->prepare("UPDATE `promos` SET `amount` = :amount WHERE `promo` = :promo");
+
+        $statement->bindValue(":promo", $promo);
+        $statement->bindValue(":amount", $amount);
+
+        $statement->execute();
+        return $this->database->changes() === 1;
+    }
+
+    public function isActivatedByUser(Player $player, string $promo): bool {
+        $name = strtolower($player->getName());
+        $statement = $this->database->prepare("SELECT * FROM `users` WHERE `name` = :name AND `promo` = :promo");
+
+        $statement->bindValue(":name", $name);
+        $statement->bindValue(":promo", $promo);
+
+        $result = $statement->execute();
+        return !empty($result->fetchArray(SQLITE3_ASSOC));
+    }
+
+    public function addToUser(Player $player, string $promo): bool {
+        $name = strtolower($player->getName());
+        $statement = $this->database->prepare("INSERT INTO `users` (`name`, `promo`) VALUES (:name, :promo)");
+
+        $statement->bindValue(":name", $name);
+        $statement->bindValue(":promo", $promo);
+
+        $statement->execute();
+        return $this->database->changes() === 1;
+    }
+
+    public function deleteFromUser(Player $player, string $promo): bool {
+        $name = strtolower($player->getName());
+        $statement = $this->database->prepare("DELETE FROM `users` WHERE `name` = :name AND `promo` = :promo");
+
+        $statement->bindValue(":name", $name);
+        $statement->bindValue(":promo", $promo);
+
+        $statement->execute();
+        return $this->database->changes() === 1;
+    }
 }
