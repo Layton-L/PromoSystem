d8505c301cb762a8f3370ac3a6ba7cd49267e80b
Demky <nemoaleksey20054@gmail.com>
2022-08-17 23:02:03 +0300

    update

---
+++ src/PromoSystem/Layton/types/PromoTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PromoSystem\Layton\types;
+
+class PromoTypes {
+
+    public const TEMPORARY = "temporary";
+
+    public const USES_LIMITED = "uses_limited";
+
+}
