f0bfd59bb569d9950d2919d5147f628e281006cc
Demky <nemoaleksey20054@gmail.com>
2022-08-17 14:17:30 +0300

    update

--- src/PromoSystem/Layton/provider/ConfigProvider.php
+++ src/PromoSystem/Layton/provider/ConfigProvider.php
@@ -11,150 +11,155 @@ use PromoSystem\Layton\PromoSystem;
 
 class ConfigProvider implements Provider {
 
-    private Config $config;
+    private Config $promos;
 
-    private array $data;
+    private Config $users;
 
     public function __construct(PromoSystem $plugin, int $type) {
         $format = array_flip(Config::$formats)[$type];
-        $this->config = new Config($plugin->getDataFolder() . "database." . $format, $type, [
-            "promos" => [],
-            "users" => []
-        ]);
 
-        $this->data = $this->config->getAll();
+        $this->promos = new Config($plugin->getDataFolder() . "promos." . $format, $type);
+        $this->users = new Config($plugin->getDataFolder() . "users." . $format, $type);
     }
 
     public function isCreated(string $promo): bool {
-        $promos = $this->data["promos"];
-        return isset($promos[$promo]);
+        return $this->promos->exists($promo);
     }
 
     public function isTemporary(string $promo): bool {
-        $promos = $this->data["promos"];
-        return $promos[$promo][1] === -1;
+        $promo = $this->promos->get($promo);
+        return $promo[1] === -1;
     }
 
     public function isUsesLimited(string $promo): bool {
-        $promos = $this->data["promos"];;
-        return $promos[$promo][2] === -1;
+        $promo = $this->promos->get($promo);
+        return $promo[2] === -1;
     }
 
     public function createTemporary(string $promo, int $actionTime, int $amount): bool {
         try {
-            $this->data["promos"][$promo] = [0, -1, $actionTime, $amount];
-            $this->config->setAll($this->data);
+            $this->promos->set($promo, [0, -1, $actionTime, $amount]);
+            $this->promos->save();
 
-            $this->config->save();
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
 
     public function createUsesLimited(string $promo, int $maxUses, int $amount): bool {
         try {
-            $this->data["promos"][$promo] = [0, $maxUses, -1, $amount];
-            $this->config->setAll($this->data);
+            $this->promos->set($promo, [0, $maxUses, -1, $amount]);
+            $this->promos->save();
 
-            $this->config->save();
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
 
     public function delete(string $promo): bool {
         try {
-            unset($this->data["promos"][$promo]);
-            $this->config->setAll($this->data);
+            $this->promos->remove($promo);
+            $this->promos->save();
 
-            $this->config->save();
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
 
     public function getUses(string $promo): int {
-        return $this->data["promos"][$promo][0];
+        return $this->promos->get($promo)[0];
     }
 
     public function setUses(string $promo, int $uses): bool {
         try {
-            $this->data["promos"][$promo][0] = $uses;
-            $this->config->setAll($this->data);
+            $data = $this->promos->get($promo);
+            $data[0] = $uses;
 
-            $this->config->save();
+            $this->promos->set($promo, $data);
+            $this->promos->save();
+
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
 
     public function getMaxUses(string $promo): int {
-        return $this->data["promos"][$promo][1];
+        return $this->promos->get($promo)[1];
     }
 
     public function setMaxUses(string $promo, int $maxUses): bool {
         try {
-            $this->data["promos"][$promo][1] = $maxUses;
-            $this->config->setAll($this->data);
+            $data = $this->promos->get($promo);
+            $data[1] = $maxUses;
 
-            $this->config->save();
+            $this->promos->set($promo, $data);
+            $this->promos->save();
+
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
 
     public function getActionTime(string $promo): int {
-        return $this->data["promos"][$promo][2];
+        return $this->promos->get($promo)[2];
     }
 
     public function setActionTime(string $promo, int $actionTime): bool {
         try {
-            $this->data["promos"][$promo][2] = $actionTime;
-            $this->config->setAll($this->data);
+            $data = $this->promos->get($promo);
+            $data[2] = $actionTime;
 
-            $this->config->save();
+            $this->promos->set($promo, $data);
+            $this->promos->save();
+
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
 
     public function getAmount(string $promo): int {
-        return $this->data["promos"][$promo][3];
+        return $this->promos->get($promo)[3];
     }
 
     public function setAmount(string $promo, int $amount): bool {
         try {
-            $this->data["promos"][$promo][3] = $amount;
-            $this->config->setAll($this->data);
+            $data = $this->promos->get($promo);
+            $data[3] = $amount;
 
-            $this->config->save();
+            $this->promos->set($promo, $data);
+            $this->promos->save();
+
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
 
     public function isActivatedByUser(Player $player, string $promo): bool {
-        $users = $this->data["users"];
-        return in_array($promo, $users[strtolower($player->getName())]);
+        $name = strtolower($player->getName());
+        $promos = $this->users->get($name);
+
+        return in_array($promo, $promos);
     }
 
     public function addToUser(Player $player, string $promo): bool {
         try {
             $name = strtolower($player->getName());
-            $this->data["users"][$name][] = $promo;
 
-            $this->config->setAll($this->data);
-            $this->config->save();
+            $promos = $this->users->get($name);
+            $promos[] = $promo;
+
+            $this->users->set($name, $promos);
+            $this->users->save();
 
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
@@ -162,14 +167,16 @@ class ConfigProvider implements Provider {
     public function deleteFromUser(Player $player, string $promo): bool {
         try {
             $name = strtolower($player->getName());
-            $key = array_search($promo, $this->data["users"][$name]);
-            unset($this->data["users"][$name][$key]);
+            $promos = $this->users->get($name);
 
-            $this->config->setAll($this->data);
-            $this->config->save();
+            $key = array_search($promo, $promos);
+            unset($promos[$key]);
+
+            $this->users->set($name, $promos);
+            $this->users->save();
 
             return true;
-        } catch (Exception $exception) {
+        } catch (Exception) {
             return false;
         }
     }
